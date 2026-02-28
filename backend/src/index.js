66a120ca347c4552a41da3e7a9a66393bebfc80c
peaga <pauloherbert@alu.ufc.br>
2026-02-28 19:46:45 -0300

    fix: optimize puppeteer flags for docker

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,17 @@ const client = new Client({
         dataPath: './data/auth'
     }),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
